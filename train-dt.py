f703e5056bcffce83eebf0c35daeccbb23e8ec27
Ranjeet-Kumar60 <ranjeetdhn60@gmail.com>
2025-07-06 20:08:28 +0530

    commit

--- train-dt.py
+++ train-dt.py
@@ -6,10 +6,16 @@ from sklearn.metrics import accuracy_score, confusion_matrix
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+import dagshub
+dagshub.init(repo_owner='Ranjeet-Kumar60', repo_name='dagshub-demo', mlflow=True)
+
+mlflow.set_tracking_uri("https://dagshub.com/Ranjeet-Kumar60/dagshub-demo.mlflow")
+
 # Load the iris dataset
 iris = load_iris()
 X = iris.data
 y = iris.target
+
 
 # Split the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
